f36fa873067da575c833ba1a0feaeabd7832fceb
Nikh13 <nikhil.loney@gmail.com>
2015-03-20 20:03:47 +0530

    added a bit of ui to help testing

--- app/src/main/java/com/karthikb351/mapable/activities/MainActivity.java
+++ app/src/main/java/com/karthikb351/mapable/activities/MainActivity.java
@@ -2,34 +2,62 @@ package com.karthikb351.mapable.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.karthikb351.mapable.Mapable;
 import com.karthikb351.mapable.R;
+import com.karthikb351.mapable.bus.BusProvider;
+import com.karthikb351.mapable.bus.events.BeaconsFoundInRange;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
+import org.altbeacon.beacon.Beacon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import timber.log.Timber;
 
 
 public class MainActivity extends ActionBarActivity {
 
     Mapable app;
+    List<Beacon> mBeaconList = new ArrayList<>();
+    BeaconListAdapter mAdapter;
+    ListView mBeaconListView;
+    private Bus mBus = BusProvider.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         app = (Mapable)getApplication();
+        mBeaconListView = (ListView)findViewById(R.id.beacons_list);
+        mAdapter = new BeaconListAdapter();
+        mBeaconListView.setAdapter(mAdapter);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         app.startBeaconService();
+        mBus.register(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         app.stopBeaconService();
+        mBus.unregister(this);
     }
 
     @Override
@@ -52,5 +80,47 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class BeaconListAdapter extends ArrayAdapter<Beacon>{
+
+        public BeaconListAdapter(){
+            super(getApplicationContext(),R.layout.beacon_item);
+        }
+
+        @Override
+        public Beacon getItem(int position) {
+            return mBeaconList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mBeaconList.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View item = null;
+            LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+            item = inflater.inflate(R.layout.beacon_item,null);
+
+            TextView beaconId = (TextView)item.findViewById(R.id.beacon_id);
+            TextView beaconDistance = (TextView)item.findViewById(R.id.beacon_distance);
+
+            Beacon beacon = getItem(position);
+
+            if(beacon != null){
+                beaconId.setText(beacon.getId1()+"");
+                beaconDistance.setText(beacon.getDistance()+"");
+            }
+
+            return item;
+        }
+    }
+
+    @Subscribe
+    public void onBeaconFoundInRange(BeaconsFoundInRange b){
+        mBeaconList = b.getBeacons();
+        mAdapter.notifyDataSetChanged();
     }
 }
